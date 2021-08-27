fda74f7321402c1935ecaf9e8e99cbd2f98249ed
JuanCastaneda2 <castaneda_juan@javeriana.edu.co>
2021-08-27 16:37:39 -0500

    Change SKU

--- data.tf
+++ data.tf
@@ -2,5 +2,5 @@ data "azurerm_platform_image" "ubuntu_image" {
   location  = var.location
   publisher = "canonical"
   offer     = "0001-com-ubuntu-server-focal"
-  sku       = "20.04-LTS"
+  sku       = "20_04-lts-ARM"
 }
